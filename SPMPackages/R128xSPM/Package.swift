4f01fea5fc9ee4ae88458cc548fb1a4151bf36e9
ShikiSuen <shikisuen@outlook.com>
2025-09-13 01:29:01 +0800

    Repo // Add iPad target, requiring iOS 17+.

--- SPMPackages/R128xSPM/Package.swift
+++ SPMPackages/R128xSPM/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "R128xSPM",
   platforms: [
-    .macOS(.v14),
+    .macOS(.v14), .iOS(.v17),
   ],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
