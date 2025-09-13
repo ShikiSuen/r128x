c2641dad8c7fb846b01601613070f43720e39a8f
ShikiSuen <shikisuen@outlook.com>
2025-09-13 15:19:41 +0800

    Repo // Update file headers.

--- SPMPackages/R128xSPM/Sources/R128xKit/MainView.swift
+++ SPMPackages/R128xSPM/Sources/R128xKit/MainView.swift
@@ -1,5 +1,4 @@
-// (c) (C ver. only) 2012-2013 Manuel Naudin (AGPL v3.0 License or later).
-// (c) (this Swift implementation) 2024 and onwards Shiki Suen (AGPL v3.0 License or later).
+// (c) 2024 and onwards Shiki Suen (AGPL v3.0 License or later).
 // ====================
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
 
